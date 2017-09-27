31c84149a8812000983aeaf32b8a8b6d371da5c3
Garcias <sergio.garcia.sti@gmail.com>
2017-09-27 16:27:50 +0200

    Renombrado de la clase a Calcudadorahija e inclusion en la excepcion ZeroDivisionError -> sys.exit

--- calcoohija.py
+++ calcoohija.py
@@ -4,52 +4,32 @@
 import sys
 import calcoo
 
-class calculadora(calcoo.calculadora):
+
+class calculadorahija(calcoo.calculadora):
     """ Aquí definimos la clase"""
-     
     def multi(self):
         return self.n1 * self.n2
-        
     def div(self):
-        return self.n1 / self.n2
-
-
+        try:
+            return self.n1 / self.n2
+        except ZeroDivisionError:
+            sys.exit("Division by zero is not allowed")
 if __name__ == "__main__":
     try:
         op1 = int(sys.argv[1])
         op2 = int(sys.argv[3])
     except ValueError:
         sys.exit("Error: Non numerical parameters")
-        
-    calculo = calculadora(op1,op2)
-    
-    try:
-        if sys.argv[2] == "suma":
-            result = calculo.plus()
-        elif sys.argv[2] == "resta":
-            result = calculo.minus()
-        elif sys.argv[2] == "multiplica":
-            result = calculo.multi()
-        elif sys.argv[2] == "divide":
-            result = calculo.div()
-        else:
-            sys.exit('Operación sólo puede ser sumar o restar.')
-        print(result)
-    except ZeroDivisionError:
-        print("Division by zero is not allowed")
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    calculo = calculadorahija(op1, op2)
+    if sys.argv[2] == "suma":
+        result = calculo.plus()
+    elif sys.argv[2] == "resta":
+        result = calculo.minus()
+    elif sys.argv[2] == "multiplica":
+        result = calculo.multi()
+    elif sys.argv[2] == "divide":
+        result = calculo.div()
+    else:
+        sys.exit('Operaciones válidas: suma,resta,multiplica y divide.')
+    print(result)
+
