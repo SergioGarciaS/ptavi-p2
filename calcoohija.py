94edcd4931cb9fd366964e84c15250a2c838df9c
Garcias <sergio.garcia.sti@gmail.com>
2017-09-27 14:46:04 +0200

    Creada la clase hija

---
+++ calcoohija.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import calcoo
+
+class calculadora(calcoo.calculdora):
+    """ Aquí definimos la clase"""
+     
+    def multi(self):
+        return self.n1 * self.n2
+        
+    def div(self):
+        try:
+            return self.n1 / self.n2
+        except ZeroDivisionError:
+            print("Division by zero is not allowed")
+
+if __name__ == "__main__":
+    try:
+        op1 = int(sys.argv[1])
+        op2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
+        
+    calculo = calculadora(op1,op2)
+    
+    if sys.argv[2] == "suma":
+       result = calculo.plus()
+    elif sys.argv[2] == "resta":
+       result = calculo.minus()
+      
+    else:
+        sys.exit('Operación sólo puede ser sumar o restar.')
+
+    print(result)
