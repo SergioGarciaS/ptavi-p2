32d2be659373ff22be5a0daf6d7fcec733dc84b1
Garcias <sergio.garcia.sti@gmail.com>
2017-09-28 18:23:56 +0200

    Esquema hecho, falta implementarlo con la clase calculadorahija

--- calcplus.py
+++ calcplus.py
@@ -10,21 +10,23 @@ calculos.close()
 
 for operaciones in lineas:
     operaciones = operaciones[:-1].split(",")
-    ops = operaciones[1:]
+    ops = operaciones[1:] #Hasta aquí el troceador.
+    
     print(operaciones[0])
     indice = 0
     resultado = int(ops[0])
-    for indice in range(len(operaciones)-1):
+    for indice in range(len(ops)-1):
 
         if operaciones[0] == "suma":
-            resultado = resultado +int(ops[indice+2])
+            resultado = resultado +int(ops[indice+1])
         elif operaciones[0] == "resta":
-            resultado = resultado -int(operaciones[indice])
+            resultado = resultado -int(ops[indice+1])
         elif operaciones[0] == "divide":
-            resultado = resultado /int(operaciones[indice])
+            resultado = resultado /int(ops[indice+1])
         elif operaciones[0] == "multiplica":
-            resultado = resultado *int(operaciones[indice])
+            resultado = resultado *int(ops[indice+1])
         else:
             sys.exit('Operaciones válidas: suma,resta,multiplica y divide.')
-        print(resultado)
+            
+    print(resultado)
 print(operaciones)
