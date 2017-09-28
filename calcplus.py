003589d3604f078bdfdf8831a97beaa967ce8ba3
Garcias <sergio.garcia.sti@gmail.com>
2017-09-28 18:48:31 +0200

    Calculadora terminada con clase calculadora

--- calcplus.py
+++ calcplus.py
@@ -3,6 +3,7 @@
 
 import sys
 import calcoo
+import calcoohija
 
 calculos = open(str(sys.argv[1]), "r")
 lineas = calculos.readlines()
@@ -10,24 +11,25 @@ calculos.close()
 
 for operaciones in lineas:
     operaciones = operaciones[:-1].split(",")
-    ops = operaciones[1:] #Hasta aquí el troceador.
-    
+    ops = operaciones[1:]  # Hasta aquí el troceador.
+
     print(operaciones[0])
     indice = 0
     resultado = int(ops[0])
     for indice in range(len(ops)-1):
         num2 = int(ops[indice+1])
+        calculo = calcoohija.calculadorahija(resultado, num2)
         if operaciones[0] == "suma":
-            resultado = resultado +num2
+            resultado = calculo.plus()
         elif operaciones[0] == "divide":
-            if num2 != 0: 
-                resultado = resultado /num2
+            if num2 != 0:
+                resultado = calculo.div()
             else:
-                sys.exit("Division by zero is not allowed") 
+                sys.exit("Division by zero is not allowed")
         elif operaciones[0] == "resta":
-            resultado = resultado -num2
+            resultado = calculo.minus()
         elif operaciones[0] == "multiplica":
-            resultado = resultado *num2
+            resultado = calculo.multi()
         else:
             sys.exit('Operaciones válidas: suma,resta,multiplica y divide.')
 
